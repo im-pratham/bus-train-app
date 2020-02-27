dceb832a20e95adac53a743bd92be2e061b20666
Jorge Mora Giménez <jorge@ge.org.es>
2020-02-27 16:48:47 +0000

    CustomFlowable Controller

---
+++ bus-train-app/src/main/java/com/bus/train/controller/CustomFlowableController.java
@@ -0,0 +1,61 @@
+package com.bus.train.controller;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+
+/*
+ * TODO:
+ * 
+ * Create REST endpoints to:
+*  - Get the list of available definitions (keys and names)
+*  - Start a new process by its key
+*  - Get the list of active tasks (task id, process id and task name)
+*  - Complete a specific task by task id
+*  - Get the image representing the current state of a process by process Id
+*  Either use a REST client (like [Postman] to interact with your API or create your own web application client interface)
+ * 
+ * 
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class CustomFlowableController {
+
+    private final RepositoryService repositoryService;
+
+    @GetMapping("/principal")
+    public String principal(Principal principal) {
+        return principal.getName();
+    }
+
+    @GetMapping("/process-definitions")
+    public List<Map<String, String>> getProcessDefinitions(Principal principal) {
+
+        List<ProcessDefinition> processDefinitionList = this.repositoryService.createProcessDefinitionQuery()
+                .latestVersion().startableByUser(principal.getName()).list();
+
+        List<Map<String, String>> customProcessDefinitionList = new ArrayList<>();
+
+        for (ProcessDefinition processDefinition : processDefinitionList) {
+            Map<String, String> map = new LinkedHashMap<>();
+            map.put("key", processDefinition.getKey());
+            map.put("name", processDefinition.getName());
+            customProcessDefinitionList.add(map);
+        }
+
+        return customProcessDefinitionList;
+
+    }
+
+}
